d2b109db523e1827fd568b1b3a722a2217fcd2ef
j3rryhu <97777602+j3rryhu@users.noreply.github.com>
2022-03-24 22:19:33 +0800

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -9,23 +9,28 @@ class LossWithEuler(nn.Module):
         self.attribute_num = torch.tensor(attributes_num) # num of samples in different attr
 
     def forward(self, inp, label, ea, attribute):
-        _, batch_size = torch.Size(inp)
-        attr_weight_mat = torch.zeros([1, batch_size])
-        angle_weight_mat = torch.zeros([1, batch_size])
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        batch_size = 32
+        attr_weight_mat = torch.zeros([batch_size, 1])
+        angle_weight_mat = torch.zeros([batch_size, 1])
+        attr_weight_mat.to(device)
+        angle_weight_mat.to(device)
         for i in range(0, batch_size):
             attr_weight = 0
             for j in range(0, 6):
-                if attribute[j][i] == 1:
-                    attr_weight += sum(self.attribute_num)/self.attribute_num[i]
-                    attr_weight_mat[1][i] = attr_weight
+                if attribute[i][j] == 1:
+                    attr_weight += sum(self.attribute_num)/self.attribute_num[j]
+                    attr_weight_mat[i][0] = attr_weight
         for i in range(0, batch_size):
             angle_weight = 0
             for j in range(0, 3):
-                angle_weight += 1 - math.cos(ea[j][i])
-                angle_weight_mat[1][i] = angle_weight
+                angle_weight += 1 - math.cos(ea[i][j])
+                angle_weight_mat[i][0] = angle_weight
         weight = torch.mul(angle_weight_mat, attr_weight_mat)
+        weight = weight.to(device)
         mseloss = torch.mean(weight*torch.pow(inp - label, 2))
         return mseloss
+
 
 
 
