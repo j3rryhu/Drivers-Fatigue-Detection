4d3841302a53333e28357273ae6f8cd3528a976d
j3rryhu <97777602+j3rryhu@users.noreply.github.com>
2022-04-05 22:17:52 +0800

    Update training.py

--- training.py
+++ training.py
@@ -1,8 +1,6 @@
-import numpy as np
 from torch.utils import data
 from PIL import Image
 from resnet import ResNet
-from torch import nn
 from torch import optim
 import torch as t
 from torchvision.transforms import ToPILImage
@@ -10,9 +8,10 @@ from torchvision import transforms
 from loss_function import LossWithEuler
 from euler_angle_calculator import EulerAngleCalc
 import os
-import cv2
+import torch
+import numpy as np
 show = ToPILImage()
-transfrom = transforms.Compose([
+transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
@@ -21,7 +20,6 @@ ann = open('./Img Dataset/annotations/train_annotation.txt')
 annotations = ann.readlines()
 attr_num = list(map(int, annotations[-7:-1]))
 euler_angle_calculation = EulerAngleCalc()
-
 
 def createDir(parentdir, dirname):
     if dirname not in os.listdir(parentdir):
@@ -48,7 +46,7 @@ class TrainFaces(data.Dataset):
                 lm[i] /= 75
                 lm[i+1] /= 100  # avoid gradient explosion
             ea = euler_angle_calculation.calculate(lm)
-            boundingbox = list(map(int, line[197: 201]))
+            boundingbox = list(map(float, line[197: 201]))
             attribute = list(map(int, line[201: 207]))
             sample = [img_path, lm, boundingbox, attribute, ea]
             self.samples.append(sample)
@@ -57,22 +55,39 @@ class TrainFaces(data.Dataset):
     def __getitem__(self, index):
         img_path = self.samples[index][0]
         img = Image.open(img_path)
-        lm = self.samples[index][1]
+        img = transform(img)
+        lm = self.samples[index][1]     # only extract the upper, lower eyelid, corner and upper, lower lips
+        train_pts = [60, 62, 64, 66, 68, 70, 72, 74, 88, 90, 92, 94]
+        training_lm = []
+        for track_pt in train_pts:
+            training_lm.append(lm[2*track_pt])
+            training_lm.append(lm[2 * track_pt+1])
+        training_lm = torch.tensor(training_lm)
         boundingbox = self.samples[index][2]
+        boundingbox = torch.tensor(boundingbox)
         attribute = self.samples[index][3]
+        attribute = torch.tensor(attribute)
         ea = self.samples[index][4]
-        return img, lm, boundingbox, attribute, ea
+        ea = np.squeeze(ea)
+        ea = torch.tensor(ea)
+        return img, training_lm, boundingbox, attribute, ea
 
     def __len__(self):
         return len(self.samples)
 
 
-trainloader = data.DataLoader(TrainFaces(transfrom), batch_size=128, shuffle=True, num_workers=0)
+trainloader = data.DataLoader(TrainFaces(transform), batch_size=32, shuffle=True, num_workers=0)
 device = t.device("cuda:0" if t.cuda.is_available() else "cpu")
 net = ResNet().to(device)
 criterion = LossWithEuler(attr_num)
-optimizer = optim.SGD(net.parameters(), lr=0.0005)
+optimizer = optim.SGD(net.parameters(), lr=5e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100, eta_min=8e-6)
 t.set_num_threads(8)
+# train on previous model
+ckpt = torch.load('./models/ckpt.pth.tar')
+state = ckpt['state']
+model = ResNet()
+model.load_state_dict(state)    # use for second round of training
 
 if t.cuda.is_available():
     pass
@@ -83,27 +98,32 @@ correct = 0.0
 total = 0.0
 count = 0
 print('all preparation done, start training')
-for epoch in range(150):
+for epoch in range(250):
     state = {
         "epoch": epoch+1,
         "state": net.state_dict(),
         "optimizer": optimizer.state_dict()
     }
+    print('-----epoch {}-----'.format(epoch))
     for i, data in enumerate(trainloader, 0):
         count += 1
         inputs, landmark, bbox, attr, euler_angle = data
+        batch_size = inputs.size(0)
         landmark = t.tensor(landmark)
-        landmark = landmark.cuda()
+        landmark = landmark.to(device)
         attr = t.tensor(attr)
-        attr = attr.cuda()
+        attr = attr.to(device)
         euler_angle = t.tensor(euler_angle)
-        euler_angle = euler_angle.cuda()
+        euler_angle = euler_angle.to(device)
         inputs = inputs.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
-        loss = criterion(outputs, landmark, euler_angle, attr)
+        loss = criterion(outputs, landmark, euler_angle, attr, batch_size)
+        assert torch.isnan(loss).sum()==0, print(loss)
         loss.backward()
         optimizer.step()
+        scheduler.step()
         if i % 100 == 0:
             print("The {} batch loss is {}".format(i, loss.item()))
     t.save(state, '.\\models\\ckpt.pth.tar')
+
